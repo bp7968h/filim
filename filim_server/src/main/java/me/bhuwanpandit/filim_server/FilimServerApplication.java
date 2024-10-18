456adc3a2dab2fc9d15c67944a233a39679c435d
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-10-19 00:03:02 +0100

    feat: initial backend setup

---
+++ filim_server/src/main/java/me/bhuwanpandit/filim_server/FilimServerApplication.java
@@ -0,0 +1,13 @@
+package me.bhuwanpandit.filim_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilimServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilimServerApplication.class, args);
+	}
+
+}
---
+++ filim_server/src/test/java/me/bhuwanpandit/filim_server/FilimServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bhuwanpandit.filim_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilimServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
