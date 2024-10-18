fe53762835ff31a07bf02869132be73664c7f35b
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-10-19 00:34:20 +0100

    chore: initial backend setup and update readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# Filim
+This is a project to learn about `java spring boot`, which is the backend server for the project. The frontend is in `react`.
+
+## Starting the Project
+
+- Clone the repository
+```bash
+git clone git@github.com:bp7968h/filim.git
+cd filim
+```
+
+- Start the backend
+
+You can run the server locally using maven.
+```bash
+cd filim_server
+./mvnw spring-boot:run
+```
+You can also package the application to jar.
+```bash
+cd filim_server
+./mvnw clean package
+java -jar target/filim_server-0.0.1.jar
+```
+
+- Start the frontend
+ 
--- filim_server/src/main/java/me/bhuwanpandit/filim_server/FilimServerApplication.java
+++ filim_server/src/main/java/me/bhuwanpandit/filim_server/FilimServerApplication.java
@@ -3,11 +3,24 @@ package me.bhuwanpandit.filim_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @SpringBootApplication
+@RestController
 public class FilimServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilimServerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String api_root() {
+		return "Hello Root!";
+	}
+	
+	@GetMapping("/hello")
+	public String hello_word() {
+		return "Hello World!";
+	}
 }
