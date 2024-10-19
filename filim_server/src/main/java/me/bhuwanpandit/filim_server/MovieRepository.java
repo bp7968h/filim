8dded2d890538b1e624e3a9573927a5f710cdd5f
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-10-19 18:30:44 +0100

    feat: add controller to get movie using imdbId

--- filim_server/src/main/java/me/bhuwanpandit/filim_server/MovieRepository.java
+++ filim_server/src/main/java/me/bhuwanpandit/filim_server/MovieRepository.java
@@ -1,5 +1,7 @@
 package me.bhuwanpandit.filim_server;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
