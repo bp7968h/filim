6a2bba463e6fdf2b128793e73202db7c5d8a9531
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-10-20 22:36:57 +0100

    refact: add cors origins to be injected from env and add a root endpoint for status

--- filim_server/src/main/java/me/bhuwanpandit/filim_server/FilimServerApplication.java
+++ filim_server/src/main/java/me/bhuwanpandit/filim_server/FilimServerApplication.java
@@ -2,11 +2,27 @@ package me.bhuwanpandit.filim_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @SpringBootApplication
+@RestController
 public class FilimServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilimServerApplication.class, args);
 	}
+
+	@GetMapping
+    public ResponseEntity<Map<String, String>> root() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "ok");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
